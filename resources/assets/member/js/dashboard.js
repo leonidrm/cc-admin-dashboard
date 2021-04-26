18094809afd66ddadd628c3ae42e81a7f733d4d2
arlad <arlad.ru@gmail.com>
2021-04-26 23:49:38 +0300

    Chart.js updated

--- resources/assets/member/js/dashboard.js
+++ resources/assets/member/js/dashboard.js
@@ -1,202 +1,50 @@
 (function ($) {
-
-    //log chart
-    var logActivity = {
-        options: {
-            date: {
-                startDate: moment().subtract(6, 'days'),
-                endDate: moment(),
-                minDate: moment().subtract(60, 'days'),
-                maxDate: moment(),
-                dateLimit: {
-                    days: 60
-                },
-                showDropdowns: true,
-                showWeekNumbers: true,
-                timePicker: false,
-                timePickerIncrement: 1,
-                timePicker12Hour: true,
-                ranges: {
-                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                    'This Month': [moment().startOf('month'), moment().endOf('month')],
-                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-                },
-                opens: 'left',
-                buttonClasses: ['btn btn-default'],
-                applyClass: 'btn-small btn-primary',
-                cancelClass: 'btn-small',
-                format: 'MM/DD/YYYY',
-                separator: ' to ',
-                locale: {
-                    applyLabel: 'Submit',
-                    cancelLabel: 'Clear',
-                    fromLabel: 'From',
-                    toLabel: 'To',
-                    customRangeLabel: 'Custom',
-                    daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-                    monthNames: [
-                        'January',
-                        'February',
-                        'March',
-                        'April',
-                        'May',
-                        'June',
-                        'July',
-                        'August',
-                        'September',
-                        'October',
-                        'November',
-                        'December'
-                    ],
-                    firstDay: 1
-                }
-            },
-            chart: {
-                series: {
-                    lines: {
-                        show: false,
-                        fill: true
-                    },
-                    splines: {
-                        show: true,
-                        tension: 0.4,
-                        lineWidth: 1,
-                        fill: 0.4
-                    },
-                    points: {
-                        radius: 0,
-                        show: true
-                    },
-                    shadowSize: 2
-                },
-                grid: {
-                    verticalLines: true,
-                    hoverable: true,
-                    clickable: true,
-                    tickColor: "#d5d5d5",
-                    borderWidth: 1,
-                    color: '#fff'
-                },
-                colors: ["#B71C1C", "#D32F2F", '#F44336', '#FF5722', '#FF9100', '#4CAF50', '#1976D2', '#90CAF9'],
-                xaxis: {
-                    tickColor: "rgba(51, 51, 51, 0.06)",
-                    mode: "time",
-                    tickSize: [1, "day"],
-                    //tickLength: 10,
-                    axisLabel: "Date",
-                    axisLabelUseCanvas: true,
-                    axisLabelFontSizePixels: 12,
-                    axisLabelFontFamily: 'Verdana, Arial',
-                    axisLabelPadding: 10
-                },
-                yaxis: {
-                    ticks: 8,
-                    tickColor: "rgba(51, 51, 51, 0.06)",
-                },
-                tooltip: false
-            }
-        },
-        gteChartData: function ($el, start, end) {
-            var self = this;
-
-            $.ajax({
-                url: 'member/dashboard/log-chart',
-                data: {start: start, end: end},
-                success: function (response) {
-                    var data = {};
-                    var progress = {all: 0};
-
-                    $.each(response, function (k, v) {
-                        data[k] = [];
-                        progress[k] = 0;
-                        $.each(v, function (date, value) {
-                            data[k].push([new Date(date).getTime(), value]);
-                            progress.all += value;
-                            progress[k] += value;
-                        });
-                    });
-
-                    $.plot($el,
-                        [data.emergency, data.alert, data.critical, data.error, data.warning, data.notice, data.info, data.debug],
-                        self.options.chart);
-
-
-                    $.each(progress, function (k, v) {
-                        var $progress = $('.progress-bar.log-' + k);
-                        if ($progress.length) {
-                            $progress.attr('data-transitiongoal', 100 / progress.all * v).progressbar();
-                        }
-                    });
-                }
-            });
-        },
-        init: function ($el) {
-            var self = this;
-
-            $el = $($el);
-
-            var $dateEl = $el.find('.date_piker');
-            var $chartEl = $el.find('.chart');
-
-            $dateEl.daterangepicker(this.options.date, function (start, end) {
-                $dateEl.find('.date_piker_label').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-            });
-
-            $dateEl.on('apply.daterangepicker', function (ev, picker) {
-                self.gteChartData($chartEl, picker.startDate.format('YYYY-MM-DD'), picker.endDate.format('YYYY-MM-DD'));
-            });
-
-            self.gteChartData($chartEl, this.options.date.startDate.format('YYYY-MM-DD'), this.options.date.endDate.format('YYYY-MM-DD'));
-        }
-    };
-
-    logActivity.init($('#log_activity'));
-
-
     var dashboardUsage = {
-        _defaults: {
-            type: 'doughnut',
-            tooltipFillColor: "rgba(51, 51, 51, 0.55)",
-            data: {
-                labels: [],
-                datasets: [{
-                    data: [],
-                    backgroundColor: [
-                        "#3498DB",
-                        "#3498DB",
-                        "#9B59B6",
-                        "#E74C3C",
-                    ],
-                    hoverBackgroundColor: [
-                        "#36CAAB",
-                        "#49A9EA",
-                        "#B370CF",
-                        "#E95E4F",
-                    ]
-                }]
-            },
-            options: {
-                legend: false,
-                responsive: false
-            }
+        defaultChartOptions: function () {
+            return {
+                type: 'bar',
+                data: {
+                    labels: [],
+                    datasets: [{
+                        data: [],
+                        backgroundColor: this.getRandomColors(100),
+                        borderRadius: 2,
+                        minBarLength: 5
+                    }]
+                },
+                options: {
+                    aspectRatio: 2,
+                    scales: {
+                        x: {
+                            display: false
+                        },
+                        y: {
+                            display: false
+                        }
+                    },
+                    plugins: {
+                        legend: {
+                            display: false
+                        },
+                        tooltip: {
+                            backgroundColor: '#2A3F54'
+                        }
+                    }
+                }
+            };
         },
-        init: function ($el, start, end) {
-            var self = this;
-            $el = $($el);
+        init: function () {
+            let self = this;
 
             $.ajax({
                 url: 'member/dashboard/company-data',
-                data: {start: start, end: end},
+                data: {},
                 success: function (data) {
 
-                    $.each(data.users, function (index, item) {
-                        if (item.id === data.currentUserId) {
-                            $('[data-username]').text(item.name);
-                        }
-                    });
-
-                    $('[data-company]').text(data.company.name);
+                    self.getPageTitle(data);
+                    self.initNewslettersRevenueChart(data);
+                    self.initNewslettersBouncesChart(data);
+                    self.initNewslettersBounceRateChart(data);
 
 
 
@@ -209,19 +57,83 @@
 
 
 
-
-
-
-
-
-
-                    //self._defaults.data.datasets[0].data = [response.registration_form, response.google, response.facebook, response.twitter];
-
-                    //new Chart($el.find('.canvasChart'), self._defaults);
                 }
             });
-        }
+        },
+
+        getRandomColors: function (number) {
+            var randomColors = [];
+            var letters = '0123456789ABCDEF';
+            var color = '';
+
+            for (var i = 0; i < number; i++) {
+                color = '#';
+
+                for (var j = 0; j < 6; j++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+
+                randomColors.push(color);
+            }
+
+            return randomColors;
+        },
+
+        getPageTitle: function (data) {
+            $.each(data.users, function (index, item) {
+                if (item.id === data.currentUserId) {
+                    $('[data-username]').text(item.name);
+                }
+            });
+
+            $('[data-company]').text(data.company.name);
+        },
+
+        initNewslettersRevenueChart: function (companyData) {
+            let chartOptions = this.defaultChartOptions();
+
+            companyData.newsletters.forEach(function(item) {
+                chartOptions.data.datasets[0].data.push(item.revenue);
+                chartOptions.data.labels.push(item.list + '\n' + item.subject);
+            });
+
+            new Chart($('#newslettersRevenueChart'), chartOptions);
+        },
+
+        initNewslettersBouncesChart: function (companyData) {
+            let chartOptions = this.defaultChartOptions();
+
+            companyData.newsletters.forEach(function(item) {
+                chartOptions.data.datasets[0].data.push(item.bounces);
+                chartOptions.data.labels.push(item.list + '\n' + item.subject);
+            });
+
+            new Chart($('#newslettersBouncesChart'), chartOptions);
+        },
+
+        initNewslettersBounceRateChart: function (companyData) {
+            let chartOptions = this.defaultChartOptions();
+
+            companyData.newsletters.forEach(function(item) {
+                chartOptions.data.datasets[0].data.push(item.bounce_rate);
+                chartOptions.data.labels.push(item.list + '\n' + item.subject);
+            });
+
+            new Chart($('#newslettersBounceRateChart'), chartOptions);
+        },
+
+
+
+
+
+
+
+
+
+
+
+
     };
 
-    dashboardUsage.init($('#registration_usage'), moment().subtract(6, 'days').format('YYYY-MM-DD'), moment().format('YYYY-MM-DD'));
+    dashboardUsage.init();
 })(jQuery);
--- resources/views/member/dashboard.blade.php
+++ resources/views/member/dashboard.blade.php
@@ -2,8 +2,80 @@
 
 @section('content')
     <!-- page content -->
+    <div class="clearfix"></div>
 
-    <h1 class="username"><span data-username></span> (<span data-company></span>)</h1>
+    <h1 class="username"><span data-username></span> - <span data-company></span></h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-lg-4">
+                <div class="x_panel">
+                    <div class="x_title clearfix">
+                        <h2>Revenue</h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+                            <li><a class="close-link"><i class="fa fa-close"></i></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="x_content">
+                        <canvas id="newslettersRevenueChart"></canvas>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-lg-4">
+                <div class="x_panel">
+                    <div class="x_title clearfix">
+                        <h2>Bounces</h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+                            <li><a class="close-link"><i class="fa fa-close"></i></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="x_content">
+                        <canvas id="newslettersBouncesChart"></canvas>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-lg-4">
+                <div class="x_panel">
+                    <div class="x_title clearfix">
+                        <h2>Bounce rate</h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+                            <li><a class="close-link"><i class="fa fa-close"></i></a></li>
+                        </ul>
+                    </div>
+
+                    <div class="x_content">
+                        <canvas id="newslettersBounceRateChart"></canvas>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
